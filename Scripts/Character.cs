4a3adcf226569f07d9df604e36b0694442e4526c
Curro <82005658+Curro-47@users.noreply.github.com>
2026-07-01 04:31:08 -0600

    2D Movement

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -1,6 +1,15 @@
 using Godot;
 using System;
 
-public partial class Character : Node
+public partial class Character : CharacterBody2D
 {
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+        inputDirection = inputDirection * 20;
+
+        Velocity = inputDirection;
+
+        MoveAndSlide();
+    }
 }
