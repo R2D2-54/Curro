0802fe45facdca2ae914932aa5a44bd4d5e31616
R2D2-54 <angelarturo548300@gmail.com>
2026-07-01 21:19:06 -0600

    gravity
    gravity

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -1,15 +1,24 @@
 using Godot;
 using System;
+using System.Numerics;
 
 public partial class Character : CharacterBody2D
 {
+
+    public float speed = 300.0f;
+    public float jumpspeed = -400.0f;
     public override void _PhysicsProcess(double delta)
     {
-        Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-        inputDirection = inputDirection * 20;
+        Godot.Vector2 velocity = Velocity;
 
-        Velocity = inputDirection;
+        velocity += GetGravity() * (float)delta;
 
+        Godot.Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
+        inputDirection = inputDirection * 400 * (float)delta;
+
+        Velocity = velocity + inputDirection;
+        
         MoveAndSlide();
     }
-}
+
+}   
