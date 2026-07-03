e5ab03a9c3466fef75d9f6db1363368ba85ab99f
Curro <82005658+Curro-47@users.noreply.github.com>
2026-07-02 22:21:42 -0600

    Simple movement, jumping, gravity and friction

--- Scripts/Character.cs
+++ Scripts/Character.cs
@@ -1,27 +1,28 @@
 using Godot;
-using System;
-using System.Diagnostics;
-using System.Numerics;
 
 public partial class Character : CharacterBody2D
 {
+    private float speed = 1600f;
 
     public override void _PhysicsProcess(double delta)
     {
-        Godot.Vector2 velocity = Velocity;
 
-        velocity += GetGravity() * (float)delta;
+        // We made our own input direction 
+        // This is a vector where X is -speed or speed depending if we press left or right
+        Vector2 inputDirection = Vector2.Zero;
+        if (Input.IsActionPressed("left")) inputDirection.X -= speed;
+        if (Input.IsActionPressed("right")) inputDirection.X += speed;
 
-        Godot.Vector2 inputDirection = Input.GetVector("left", "right", "up", "down");
-        inputDirection = inputDirection * 400 * (float)delta;
+        // This is the acceleration, it uses the input direction and gravity
+        // It takes into account delta time, meaning that its now in p/s instead of p/frame
+        Vector2 acceleration = (inputDirection + GetGravity()) * (float)delta;
 
-        if (Input.IsActionJustReleased("left"))
-        {
-        
-        
-        }
+        // Jump logic, its applied to velocity instead of acceleration because its a single impulse, instead of over time
+        if (Input.IsActionJustPressed("up") && IsOnFloor()) Velocity += new Vector2(0, -500);
 
-        Velocity = velocity + inputDirection;
+        // Applying the acceleration to the Velocity and apply air resistance
+        Velocity += acceleration;
+        Velocity *= new Vector2(0.95f, 1f);
         
         MoveAndSlide();
     }
